9d29322714250f3563cfd135cbeae4da9f59c5c6
Sellieee <sellieee@hotmail.com>
2019-09-13 22:04:09 +1000

    added basic outline of server.js

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
+// Get the dependencies we need and run the necessary npm packages.
 var express = require("express");
 var path = require("path");
+
+// Configure Express
+var app = express();
+
+//Configure the Port
+var PORT = process.env.PORT || 8090;
+
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// Parsing the data
+app.use(express.urlencoded({
+   extended: true
+}));
+app.use(express.json());
+
+// Displaying the route files
+require(path.join(__dirname, "./app/routing/apiRoute"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+
+// Start listening on the port
+app.listen(PORT, function () {
+   console.log("Friend Finder is listening on PORT: " + PORT);
+});
